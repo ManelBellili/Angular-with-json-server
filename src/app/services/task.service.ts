efbafd280930cafe300c3da182d239fc0f95e4e6
ManelBellili <manelbellili2014@gmail.com>
2020-05-18 17:31:36 +0200

    first commit

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Task } from 'src/app/models/task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  apiUrl="http://localhost:5000/tasks";
+
+  constructor(private http:HttpClient) { }
+
+  findAll(){
+   return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  delete(id){
+    return this.http.delete(`http://localhost:5000/tasks/${id}`);
+}
+
+persist(task){
+  return this.http.post<Task[]>(this.apiUrl,task);
+}
+
+completed(id,completed){
+  return this.http.patch(`http://localhost:5000/tasks/${id}`,{completed : !completed});
+
+}
+
+update(task){
+  return this.http.put(`${this.apiUrl}/${task.id}`,task);
+
+}
+}
